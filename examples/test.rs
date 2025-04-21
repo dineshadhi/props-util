a71a5a72ef213b7575b801e442cdb3fde5b8e596
Dinesh Adhithya <dinesh10c04@gmail.com>
2025-04-21 22:09:48 +0530

    Added README file

--- examples/test.rs
+++ examples/test.rs
@@ -2,11 +2,12 @@ use props_util::Properties;
 use std::{fs::File, io::Read};
 
 #[derive(Properties, Debug)]
+#[allow(unused)]
 struct TestProp {
     #[prop(key = "name", default = "Dumeel")]
     name: String,
 
-    #[prop(key = "dept")]
+    #[prop(key = "dept", default = "wms")]
     dept: String,
 
     #[prop(key = "id", default = "323423")]
@@ -14,6 +15,6 @@ struct TestProp {
 }
 
 fn main() {
-    let test = TestProp::new("test.properties").unwrap();
+    let test = TestProp::from_file("test.properties").unwrap();
     dbg!(test);
 }
