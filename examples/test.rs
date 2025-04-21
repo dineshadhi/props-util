fae05127bf22e32feb56f1ce60538c28679ee6e6
Dinesh Adhithya <dinesh10c04@gmail.com>
2025-04-21 17:35:08 +0530

    initial commit

---
+++ examples/test.rs
@@ -0,0 +1,19 @@
+use props_rs::Properties;
+use std::{fs::File, io::Read};
+
+#[derive(Properties, Debug)]
+struct TestProp {
+    #[prop(key = "name", default = "Dumeel")]
+    name: String,
+
+    #[prop(key = "dept")]
+    dept: String,
+
+    #[prop(key = "id", default = "323423")]
+    empid: u32,
+}
+
+fn main() {
+    let test = TestProp::new("test.properties").unwrap();
+    dbg!(test);
+}
